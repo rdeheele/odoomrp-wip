732581d893a6b2426583620a2d7cbbc4e7002c73
Romain Deheele <rdeheele@users.noreply.github.com>
2015-06-24 11:25:21 +0200

    [UPD] work on variants_types

--- mrp_product_variants_types/models/mrp.py
+++ mrp_product_variants_types/models/mrp.py
@@ -34,3 +34,33 @@ class SaleOrderLineAttribute(models.Model):
     @api.onchange('custom_value', 'value')
     def _onchange_custom_value(self):
         self._custom_value_in_range()
+
+
+class MrpProductionProductLineAttribute(models.Model):
+    _inherit = 'mrp.production.product.line.attribute'
+
+    custom_value = fields.Float(string='Custom value')
+    attr_type = fields.Selection(string='Type', store=False,
+                                 related='attribute.attr_type')
+
+    def _is_custom_value_in_range(self):
+        if self.attr_type == 'range':
+            if not (self.value.min_range <= self.custom_value and
+                    self.value.max_range >= self.custom_value):
+                return False
+        return True
+
+    @api.one
+    @api.constrains('custom_value', 'attr_type', 'value')
+    def _custom_value_in_range(self):
+        if not self._is_custom_value_in_range():
+            raise exceptions.Warning(
+                _("Custom value for attribute '%s' must be between %s and"
+                  " %s.")
+                % (self.attribute.name, self.value.min_range,
+                   self.value.max_range))
+
+    @api.one
+    @api.onchange('custom_value', 'value')
+    def _onchange_custom_value(self):
+        self._custom_value_in_range()
